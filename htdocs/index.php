2355d383728a971121470b2d4a8ca6e3de244f2f
Matt Watt <matt@karwalski.net>
2020-04-09 21:45:13 +1000

    Moved index to htdocs directory

---
+++ htdocs/index.php
@@ -0,0 +1,24 @@
+<?PHP
+
+// Placeholder for classparty main page
+
+
+?>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>Classparty</title>
+  <meta name="description" content="Classparty">
+  <meta name="author" content="karwalski">
+
+</head>
+
+<body>
+	Welcome!<BR />
+	
+	
+	This is a placeholder page for the ACS #flattenthecurvehack challenge
+</body>
+</html>
