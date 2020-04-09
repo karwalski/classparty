53678e439ac63824775f8e2cc57c06df50c9c04e
Matt Watt <matt@karwalski.net>
2020-04-09 22:56:44 +1000

    testing google oauth for classrooms

--- htdocs/index.php
+++ htdocs/index.php
@@ -17,8 +17,43 @@
 
 <body>
 	Welcome!<BR />
+	<BR />
+	<BR />
+	This is a placeholder page for the ACS #flattenthecurvehack challenge<BR />
+	<BR />
+	<a href="" onClick="oauthSignIn">Test Google Auth</A><BR />
 	
-	
-	This is a placeholder page for the ACS #flattenthecurvehack challenge
+	<script>
+// Google OAuth2.0
+function oauthSignIn() {
+  var oauth2Endpoint = 'https://accounts.google.com/o/oauth2/v2/auth';
+
+  // Create <form> element to submit parameters to OAuth 2.0 endpoint.
+  var form = document.createElement('form');
+  form.setAttribute('method', 'GET'); // Send as a GET request.
+  form.setAttribute('action', oauth2Endpoint);
+
+  // Parameters to pass to OAuth 2.0 endpoint.
+  var params = {'client_id': '522572922800-m16cu28278v69hud1po3v8kt3f7atfeo.apps.googleusercontent.com',
+                'redirect_uri': 'https://classparty.net/?',
+                'response_type': 'token',
+                'scope': 'https://www.googleapis.com/auth/drive.metadata.readonly',
+                'include_granted_scopes': 'true',
+                'state': 'pass-through value'};
+
+  // Add form parameters as hidden input values.
+  for (var p in params) {
+    var input = document.createElement('input');
+    input.setAttribute('type', 'hidden');
+    input.setAttribute('name', p);
+    input.setAttribute('value', params[p]);
+    form.appendChild(input);
+  }
+
+  // Add form to page and submit it to open the OAuth 2.0 endpoint.
+  document.body.appendChild(form);
+  form.submit();
+}
+	</script>
 </body>
 </html>
