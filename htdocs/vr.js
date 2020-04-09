179f1d16dbad2c329c0cc1abbd389df4f3e1add0
Matt Watt <matt@karwalski.net>
2020-04-10 07:56:55 +1000

    removed blank

--- htdocs/vr.js
+++ htdocs/vr.js
@@ -42,7 +42,7 @@ function onLoad() {
   vrView = new VRView.Player('#vrview', {
     width: '100%',
     height: 480,
-    image: 'blank.png',
+    image: 'petra.jpg',
     is_stereo: false,
     is_autopan_off: true
   });
