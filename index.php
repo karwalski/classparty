c8291fcf9117aaa21d8164c02a47dc4b9bd1c060
Matt Watt <matt@karwalski.net>
2020-04-09 21:34:46 +1000

    placeholder index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<?PHP
+
+// Placeholder for classparty main page
+
+
+?>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>Classparty</title>
+  <meta name="description" content="Classparty">
+  <meta name="author" content="karwalski">
+
+</head>
+
+<body>
+	Welcome!
+</body>
+</html>
