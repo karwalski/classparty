658025d30b01297de4a5124340ab8ad05bfeefb8
Matt Watt <matt@karwalski.net>
2020-04-10 23:10:32 +1000

    translation

--- htdocs/ar.php
+++ htdocs/ar.php
@@ -65,7 +65,8 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
       // The solar system is big (citation needed). Scale it down so that users
       // can move around the planets more easily.
       // textCP.scale = [0.1, 0.1, 0.1];
-      textCP.rotation = [1, 1, 0, 0];
+      // textCP.rotation = [0, 1, 0, 0];
+      textCP.translation = [1, 0, 0];
       
 
       
