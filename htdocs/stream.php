c53b521f3c5e288ea44058580fb377672477f5e7
Matt Watt <matt@karwalski.net>
2020-04-11 07:29:59 +1000

    restest JSON

--- htdocs/stream.php
+++ htdocs/stream.php
@@ -1,7 +1,7 @@
 <?PHP
 
-$data = file_get_contents('php://input');
-echo $data;
+$data = json_decode(file_get_contents('php://input'), true);
+echo $data['imageData'];
 
 // $img = imagecreatefromstring(base64_decode($data['imageData']));
 // and process $img with your image library here
