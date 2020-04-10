699230441ca89b56ff586e04f8590a7e0a96d0a8
Matt Watt <matt@karwalski.net>
2020-04-10 23:32:13 +1000

    rotate matrix

--- htdocs/ar.php
+++ htdocs/ar.php
@@ -68,7 +68,11 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
       // can move around the planets more easily.
       // textCP.scale = [0.1, 0.1, 0.1];
       // textCP.rotation = [0, 1, 0, 0];
-      textCP.translation =  vec3.clone([1, 1, 1]);
+      textCP.translation =  [
+        [0, -1, 0, 0],
+        [1, 0, 0, 0],
+        [0, 0, 1, 0],
+        [0, 0, 0, 1]]
       
 
       
