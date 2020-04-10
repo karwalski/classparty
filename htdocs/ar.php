2ff8d12af3e4d45a5df15ad7bf035d36c61bc8bc
Matt Watt <matt@karwalski.net>
2020-04-10 22:09:55 +1000

    transform text not scene

--- htdocs/ar.php
+++ htdocs/ar.php
@@ -69,15 +69,16 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
       // The solar system is big (citation needed). Scale it down so that users
       // can move around the planets more easily.
       // solarSystem.scale = [0.1, 0.1, 0.1];
-      scene.addNode(textCP);
       
-      scene.matrix = new Float32Array([
+      
+      textCP.matrix = new Float32Array([
       1, 0, 0, 0,
       0, 1, 0, 0,
       0, 0, 1, 0,
       -0.3625, 0.3625, 0.02, 1,
     ]);
       
+      scene.addNode(textCP);
       
       // Still adding a skybox, but only for the benefit of the inline view.
       
