e26dd83be3b6ba4a5dc8b107f9ed133367595c36
Matt Watt <matt@karwalski.net>
2020-04-11 07:21:42 +1000

    test

--- htdocs/stream.php
+++ htdocs/stream.php
@@ -1,9 +1,9 @@
 <?PHP
 
-$data = json_decode(file_get_contents('php://input'), true);
-echo "{imageData:" + $data['imageData'] + "}";
+$data = file_get_contents('php://input');
+echo $data['imageData'];
 
-$img = imagecreatefromstring(base64_decode($data['imageData']));
+// $img = imagecreatefromstring(base64_decode($data['imageData']));
 // and process $img with your image library here
 
 
