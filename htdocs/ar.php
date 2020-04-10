a4b0a195c711f7d0b764dc85e6ba7c36f646b1e3
Matt Watt <matt@karwalski.net>
2020-04-10 22:26:07 +1000

    rotate y

--- htdocs/ar.php
+++ htdocs/ar.php
@@ -69,7 +69,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
       // The solar system is big (citation needed). Scale it down so that users
       // can move around the planets more easily.
       
-      textCP.rotation.z += 0.5;
+      textCP.rotation.y += 0.5;
       
 
       
