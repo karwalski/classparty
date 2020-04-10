5a2b02299331153868d76b5bf29d252750ac6679
Matt Watt <matt@karwalski.net>
2020-04-10 22:21:20 +1000

    t

--- htdocs/ar.php
+++ htdocs/ar.php
@@ -68,14 +68,14 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
       let textCP = new Gltf2Node({url: 'media/gltf/classparty.gltf'});
       // The solar system is big (citation needed). Scale it down so that users
       // can move around the planets more easily.
-      textCP.scale = [0.1, 0.1, 0.1];
+      // textCP.scale = [0.1, 0.1, 0.1];
       // textCP.translate = [100, 100, 100];
       
       textCP.matrix = new Float32Array([
       0.5, 0, 0, 0,
       0, 0.5, 0, 0,
       0, 0, 0.5, 0,
-      -0.3625, 0.3625, 0.02, 1,
+      0, 0, 0, 0.5,
     ]);
       
 
