500cdd6603f132da88d9af5d7ebdd9684868a352
Matt Watt <matt@karwalski.net>
2020-04-11 00:25:55 +1000

    colours

--- htdocs/js/face-api/face-api.js
+++ htdocs/js/face-api/face-api.js
@@ -2709,10 +2709,10 @@
           var _a = options.drawLines, drawLines = _a === void 0 ? true : _a, _b = options.drawPoints, drawPoints = _b === void 0 ? true : _b, lineWidth = options.lineWidth, lineColor = options.lineColor, pointSize = options.pointSize, pointColor = options.pointColor;
           this.drawLines = drawLines;
           this.drawPoints = drawPoints;
-          this.lineWidth = lineWidth || 1;
-          this.pointSize = pointSize || 2;
-          this.lineColor = lineColor || 'rgba(0, 255, 255, 1)';
-          this.pointColor = pointColor || 'rgba(255, 0, 255, 1)';
+          this.lineWidth = 2; // lineWidth || 1;
+          this.pointSize = 2; // pointSize || 2;
+          this.lineColor = 'rgba(239,91,91, 1)'; // lineColor || 
+          this.pointColor = 'rgba(35,0,30, 1)'; // pointColor || 
       }
       return DrawFaceLandmarksOptions;
   }());
