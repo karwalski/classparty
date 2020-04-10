564a3128f456fad48442ccdf203fb8d34db1e5c0
Matt Watt <matt@karwalski.net>
2020-04-11 06:53:30 +1000

    simple echo

--- htdocs/stream.php
+++ htdocs/stream.php
@@ -1,7 +1,8 @@
 <?PHP
 
+$imageData = $_POST['imgageData'];
 $img = imagecreatefromstring(base64_decode($imageData));
 // and process $img with your image library here
-echo print_r($img);
+echo $imageData;
 
 ?>
