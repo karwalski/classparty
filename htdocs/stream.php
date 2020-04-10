bc88268beb439eb66ec204669bb0dcbe2b59ebc7
Matt Watt <matt@karwalski.net>
2020-04-11 07:19:11 +1000

    json decode input

--- htdocs/stream.php
+++ htdocs/stream.php
@@ -1,9 +1,9 @@
 <?PHP
 
-$imageData = $_POST['imageData'];
-echo "{imageData:" + $imageData + "}";
+$data = json_decode(file_get_contents('php://input'), true);
+echo "{imageData:" + $data['imageData'] + "}";
 
-$img = imagecreatefromstring(base64_decode($imageData));
+$img = imagecreatefromstring(base64_decode($data['imageData']));
 // and process $img with your image library here
 
 
