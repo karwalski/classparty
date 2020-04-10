8fca8417eaf78d0cdccb9b3f3c735e064029d371
Matt Watt <matt@karwalski.net>
2020-04-10 16:41:27 +1000

    location from uri

--- htdocs/vr.php
+++ htdocs/vr.php
@@ -69,7 +69,20 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
       let renderer = null;
       let scene = new Scene();
       scene.addNode(new Gltf2Node({url: 'media/gltf/classparty.gltf'}));
-      scene.addNode(new SkyboxNode({url: 'chichen-itza.jpg'}));
+      
+      var params = new URLSearchParams(document.location.search);
+      
+      if(params.get("location")})
+      {
+        scene.addNode(new SkyboxNode({url: params.get("location")}));
+      }
+      else
+      {
+        scene.addNode(new SkyboxNode({url: 'chichen-itza.jpg'}));
+      }
+      
+      
+      
 
       // Checks to see if WebXR is available and, if so, queries a list of
       // XRDevices that are connected to the system.
